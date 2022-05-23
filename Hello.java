0b1802598e250af173ac58e1997497c88aaaf847
Avani14 <trivediavani14@gmail.com>
2022-05-23 10:09:32 +0530

    first commit

---
+++ Hello.java
@@ -0,0 +1,13 @@
+import java.util.Arrays;
+
+public class Hello{
+	public static void main(String[] args){
+		//System.out.println("Hello Everyone...."+args[0]+" "+args[1]);
+		System.out.println(args.length);
+		for(int i =0;i<args.length;i++)
+		{
+			System.out.println(args[i]);
+			}
+	}
+
+}
---
+++ Hello_Batch_156/src/HelloWorld.java
@@ -0,0 +1,17 @@
+
+public class HelloWorld {
+// when we want to call a method without making an object. then static
+//String arg is command line argument
+//jvm only understands main method
+//java always reads the string data
+//we need a public to access the method , anyone should be able to access this method
+	public static void main(String[] args)
+	{
+		
+		System.out.print("Hello Word!!"+args[0]);
+		System.out.print("Avani");
+		
+	}
+	
+//	
+}
